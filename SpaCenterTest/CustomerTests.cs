5a9c4f858b991d21de02e4ec712a14a4dd8374da
Mikoli2334 <mamontovadiana3688339@gmail.com>
2025-11-19 18:36:25 +0100

    tests32

--- SpaCenterTest/CustomerTests.cs
+++ SpaCenterTest/CustomerTests.cs
@@ -1,13 +1,78 @@
-using System;
 using NUnit.Framework;
 using SpaCenter;
+using System;
 
 namespace SpaCenterTest
 {
     [TestFixture]
     public class CustomerTests
     {
-        
+        private string _name = null!;
+        private string _surname = null!;
+        private string _email = null!;
+        private string _phone = null!;
+        private DateTime _dob;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _name = "Anna";
+            _surname = "Nowak";
+            _email = "anna@example.com";
+            _phone = "123456789";
+            _dob = new DateTime(2000, 1, 1);
+        }
+
+    
+        [Test]
+        public void Constructor_SetsNameCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.Name, Is.EqualTo(_name));
+        }
+
+        [Test]
+        public void Constructor_SetsSurnameCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.Surname, Is.EqualTo(_surname));
+        }
+
+        [Test]
+        public void Constructor_SetsEmailCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.Email, Is.EqualTo(_email));
+        }
+
+        [Test]
+        public void Constructor_SetsPhoneCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.PhoneNumber, Is.EqualTo(_phone));
+        }
+
+        [Test]
+        public void Constructor_SetsDateOfBirthCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.DateOfBirth, Is.EqualTo(_dob));
+        }
+
+      
+        [Test]
+        public void Constructor_ThrowsException_WhenBirthDateInFuture()
+        {
+            var future = DateTime.Today.AddDays(1);
+
+            var ex = Assert.Throws<ArgumentException>(() =>
+                new Customer(_name, _surname, _email, _phone, future));
+
+            Assert.That(ex!.Message, Is.EqualTo("Birth date can't be in the future"));
+        }
+
+
+
         [Test]
         public void BirthDateInFuture()
         {
@@ -19,7 +84,7 @@ namespace SpaCenterTest
                     surname: "Smith",
                     email: "anna@example.com",
                     phoneNumber: "123456789",
-                    dateOfBirth: futureBirthDate   
+                    dateOfBirth: futureBirthDate
                 ));
 
             Assert.That(ex.Message, Is.EqualTo("Birth date can't be in the future"));
