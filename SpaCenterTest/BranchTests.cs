5dcefcace29893d626a248e994e4c5a90e07acaf
s28977 <s28977@pjwstk.edu.pl>
2025-11-19 16:04:54 +0100

    Added BookingTests.cs, BranchTests.cs, and SerivceTests.cs. For now they only test attributes setting and exception throwing.

---
+++ SpaCenterTest/BranchTests.cs
@@ -0,0 +1,116 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using SpaCenter;
+
+namespace SpaCenterTest
+{
+    [TestFixture]
+    public class BranchTests
+    {
+        private Address _address = null!;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _address = new Address("Main Street", 10, "Warsaw", "00-001", "Poland");
+        }
+
+        // --------------------------------------------------------------
+        // Constructor attribute tests
+        // --------------------------------------------------------------
+
+        [Test]
+        public void Constructor_SetsNameCorrectly()
+        {
+            var b = new Branch("SPA Warsaw", _address, new List<string> { "+48123456789" });
+            Assert.That(b.Name, Is.EqualTo("SPA Warsaw"));
+        }
+
+        [Test]
+        public void Constructor_SetsAddressCorrectly()
+        {
+            var b = new Branch("SPA Krakow", _address, new List<string> { "+48123456789" });
+            Assert.That(b.Address, Is.EqualTo(_address));
+        }
+
+        [Test]
+        public void Constructor_SetsPhoneNumbersCorrectly()
+        {
+            var numbers = new List<string> { "+48123456789", "123456789" };
+            var b = new Branch("SPA Gdansk", _address, numbers);
+
+            Assert.That(b.PhoneNumbers, Is.EquivalentTo(numbers));
+        }
+
+        [Test]
+        public void OpeningTime_IsInitializedCorrectly()
+        {
+            Assert.That(Branch.OpeningTime, Is.EqualTo(new TimeSpan(9, 0, 0)));
+        }
+
+        [Test]
+        public void ClosingTime_IsInitializedCorrectly()
+        {
+            Assert.That(Branch.ClosingTime, Is.EqualTo(new TimeSpan(21, 0, 0)));
+        }
+
+        // --------------------------------------------------------------
+        // Constructor exception tests
+        // --------------------------------------------------------------
+
+        [Test]
+        public void Constructor_ThrowsException_WhenNameEmpty()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+                new Branch("", _address, new List<string> { "+48123456789" }));
+
+            Assert.That(ex!.Message, Does.Contain("Name can't be empty"));
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_WhenAddressNull()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+                new Branch("SPA", null!, new List<string> { "+48123456789" }));
+
+            Assert.That(ex!.ParamName, Is.EqualTo("address"));
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_WhenPhoneNumbersNull()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+                new Branch("SPA", _address, null!));
+
+            Assert.That(ex!.Message, Does.Contain("Phone numbers can't be empty"));
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_WhenPhoneNumberEmpty()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+                new Branch("SPA", _address, new List<string> { "" }));
+
+            Assert.That(ex!.Message, Does.Contain("Phone number can't be empty"));
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_WhenPhoneNumberInvalid()
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>
+                new Branch("SPA", _address, new List<string> { "abc123" }));
+
+            Assert.That(ex!.Message, Is.EqualTo("Invalid phone number"));
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_WhenNoPhoneNumbersProvided()
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>
+                new Branch("SPA", _address, new List<string>()));
+
+            Assert.That(ex!.Message, Is.EqualTo("At least one phone number is required"));
+        }
+    }
+}
--- SpaCenterTest/ServiceTests.cs
+++ SpaCenterTest/ServiceTests.cs
@@ -50,4 +50,103 @@ public class ServiceTests
 
     */
 
+namespace SpaCenterTest
+{
+    [TestFixture]
+    public class ServiceTests
+    {
+        // --------------------------------------------------------------
+        // Constructor attribute tests
+        // --------------------------------------------------------------
+
+        [Test]
+        public void Constructor_SetsNameCorrectly()
+        {
+            var s = new Service("Massage", "Relaxing massage", TimeSpan.FromMinutes(60), 200m, 16);
+            Assert.That(s.Name, Is.EqualTo("Massage"));
+        }
+
+        [Test]
+        public void Constructor_SetsDescriptionCorrectly()
+        {
+            var s = new Service("Massage", "Relaxing massage", TimeSpan.FromMinutes(60), 200m, 16);
+            Assert.That(s.Description, Is.EqualTo("Relaxing massage"));
+        }
+
+        [Test]
+        public void Constructor_SetsDurationCorrectly()
+        {
+            var duration = TimeSpan.FromMinutes(45);
+            var s = new Service("Peeling", "Skin peeling", duration, 150m, 18);
+            Assert.That(s.Duration, Is.EqualTo(duration));
+        }
+
+        [Test]
+        public void Constructor_SetsPriceCorrectly()
+        {
+            var s = new Service("Mask", "Face mask", TimeSpan.FromMinutes(30), 99.99m, 12);
+            Assert.That(s.Price, Is.EqualTo(99.99m));
+        }
+
+        [Test]
+        public void Constructor_SetsMinimalAgeCorrectly()
+        {
+            var s = new Service("Sauna", "Steam sauna", TimeSpan.FromMinutes(20), 50m, 18);
+            Assert.That(s.MinimalAge, Is.EqualTo(18));
+        }
+
+        // --------------------------------------------------------------
+        // Constructor exception tests
+        // --------------------------------------------------------------
+
+        [Test]
+        public void Constructor_ThrowsException_WhenNameIsEmpty()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+                new Service("", "Desc", TimeSpan.FromMinutes(10), 10m, 1));
+
+            Assert.That(ex!.Message, Does.Contain("Name can't be empty"));
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_WhenDescriptionIsEmpty()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+                new Service("Test", "", TimeSpan.FromMinutes(10), 10m, 1));
+
+            Assert.That(ex!.Message, Does.Contain("Description can't be empty"));
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_WhenDurationZeroOrNegative()
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>
+                new Service("Test", "Desc", TimeSpan.Zero, 10m, 1));
+
+            Assert.That(ex!.Message, Is.EqualTo("Duration can't be zero"));
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_WhenPriceZeroOrNegative()
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>
+                new Service("Test", "Desc", TimeSpan.FromMinutes(10), 0m, 1));
+
+            Assert.That(ex!.Message, Is.EqualTo("Price can't be zero or negative"));
+        }
+
+        [Test]
+        public void Constructor_ThrowsException_WhenMinimalAgeZeroOrNegative()
+        {
+            var ex = Assert.Throws<ArgumentException>(() =>
+                new Service("Test", "Desc", TimeSpan.FromMinutes(10), 10m, 0));
+
+            Assert.That(ex!.Message, Is.EqualTo("Age can't be zero or negative"));
+        }
+    }
+}
+
+
+
+
 
