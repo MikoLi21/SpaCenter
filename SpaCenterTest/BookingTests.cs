b634bdab98505ac732cec7168db517a4df587e43
tiurinatamila <s30844@pjwstk.edu.pl>
2025-11-20 05:18:47 +0100

    extent test

--- SpaCenterTest/BookingTests.cs
+++ SpaCenterTest/BookingTests.cs
@@ -17,8 +17,11 @@ namespace SpaCenterTest
         [SetUp]
         public void SetUp()
         {
+            Booking.LoadExtent(new List<Booking>());
+
             _cust = new Customer("Anna", "Nowak", "anna@example.com", "111222333", new DateTime(2000, 1, 1));
-            _emp  = new Employee("Eva", "Kowalska", "eva@example.com", "999888777", 12345678901, DateTime.Today.AddYears(-5), 4);
+            _emp = new Employee("Eva", "Kowalska", "eva@example.com", "999888777", 12345678901,
+                DateTime.Today.AddYears(-5), 4);
             _svc = new Service("Massage", "Relaxing massage", TimeSpan.FromMinutes(60), 200m, 16);
         }
 
@@ -112,6 +115,40 @@ namespace SpaCenterTest
             
             //updates instance
             Assert.That(extent[0].PaymentMethod, Is.EqualTo(PaymentMethod.PaymentGateway));
+        }
+        
+        // --------------------------------------------------------------
+        // Extent test
+        // --------------------------------------------------------------
+        
+        
+        [Test]
+        public void Extent_StoresAllCreatedBookings()
+        {
+            // Arrange
+            var b1 = new Booking(_cust, _emp, _svc, DateTime.Today.AddDays(1), PaymentMethod.AtTheSPA);
+            var b2 = new Booking(_cust, _emp, _svc, DateTime.Today.AddDays(2), PaymentMethod.AtTheSPA);
+
+            // Assert
+            Assert.That(Booking.Bookings.Count, Is.EqualTo(2));
+            Assert.That(Booking.Bookings, Does.Contain(b1));
+            Assert.That(Booking.Bookings, Does.Contain(b2));
+        }
+        
+        [Test]
+        public void CreatingBooking_WithInvalidDate_ThrowsException_AndIsNotAddedToExtent()
+        {
+            // Arrange
+            var invalidDate = DateTime.Today.AddDays(-2);
+
+            // Act & Assert – constructor should throw
+            var ex = Assert.Throws<ArgumentException>(() =>
+                new Booking(_cust, _emp, _svc, invalidDate, PaymentMethod.AtTheSPA));
+
+            Assert.That(ex!.Message, Is.EqualTo("Booking can't be planned on date earlier than today"));
+
+            // Extent should remain empty
+            Assert.That(Booking.Bookings.Count, Is.EqualTo(0));
         }
     }
 }
--- SpaCenterTest/CustomerTests.cs
+++ SpaCenterTest/CustomerTests.cs
@@ -7,6 +7,70 @@ namespace SpaCenterTest
     [TestFixture]
     public class CustomerTests
     {
+        private string _name = null!;
+        private string _surname = null!;
+        private string _email = null!;
+        private string _phone = null!;
+        private DateTime _dob;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Customer.LoadExtent(new List<Customer>());
+            _name = "Anna";
+            _surname = "Nowak";
+            _email = "anna@example.com";
+            _phone = "123456789";
+            _dob = new DateTime(2000, 1, 1);
+        }
+
+    
+        [Test]
+        public void Constructor_SetsNameCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.Name, Is.EqualTo(_name));
+        }
+
+        [Test]
+        public void Constructor_SetsSurnameCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.Surname, Is.EqualTo(_surname));
+        }
+
+        [Test]
+        public void Constructor_SetsEmailCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.Email, Is.EqualTo(_email));
+        }
+
+        [Test]
+        public void Constructor_SetsPhoneCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.PhoneNumber, Is.EqualTo(_phone));
+        }
+
+        [Test]
+        public void Constructor_SetsDateOfBirthCorrectly()
+        {
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+            Assert.That(c.DateOfBirth, Is.EqualTo(_dob));
+        }
+
+      
+        [Test]
+        public void Constructor_ThrowsException_WhenBirthDateInFuture()
+        {
+            var future = DateTime.Today.AddDays(1);
+
+            var ex = Assert.Throws<ArgumentException>(() =>
+                new Customer(_name, _surname, _email, _phone, future));
+
+            Assert.That(ex!.Message, Is.EqualTo("Birth date can't be in the future"));
+        }
         
         [Test]
         public void BirthDateInFuture()
@@ -43,12 +107,11 @@ namespace SpaCenterTest
         [Test]
         public void Modifying_Property_Should_Update_Object_Inside_Extent()
         {
-            var c = new Customer("John", "Smith", "john@test.com", "+48123456789", new DateTime(1990, 1, 1));
-            
+            var c = new Customer(_name, _surname, _email, _phone, _dob);
+
             c.DateOfBirth = new DateTime(1985, 5, 5);
 
             var first = Customer.Customers[0];
-
             Assert.That(first.DateOfBirth, Is.EqualTo(new DateTime(1985, 5, 5)));
         }
     }
