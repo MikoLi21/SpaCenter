601ebf436eb712e009a6e7ac6fabc7eb039eef10
s28977 <s28977@pjwstk.edu.pl>
2025-12-17 21:40:50 +0100

    Added 3 classes: Junior, Mid and Senior and association between Mid and Junior.

---
+++ SpaCenter/Mid.cs
@@ -0,0 +1,64 @@
+﻿namespace SpaCenter;
+
+public class Mid
+{
+    private static List<Mid> mids_List = new List<Mid>();
+    public static IReadOnlyList<Mid> Mids => mids_List.AsReadOnly();
+    private HashSet<Junior> _supervisedJuniors = new HashSet<Junior>();
+    public IEnumerable<Junior> SupervisedJuniors => _supervisedJuniors.ToHashSet();
+    
+    public Mid()
+    {
+        AddMid(this);
+    }
+
+    private static void AddMid(Mid mid)
+    {
+        if (mid == null)
+            throw new ArgumentException("Mid cannot be null");
+
+        mids_List.Add(mid);
+    }
+
+    public static void LoadExtent(IEnumerable<Mid>? list)
+    {
+        mids_List.Clear();
+
+        if (list == null) return;
+
+        mids_List.AddRange(list);
+    }
+    
+    public void AddJuniorToMid(Junior junior)
+    {
+        if (junior == null)
+            throw new ArgumentNullException(nameof(junior));
+
+        if (_supervisedJuniors.Contains(junior))
+            return; 
+
+        _supervisedJuniors.Add(junior);
+        junior.AddMidToJuniorReverse(this);  
+    }
+    public void RevomeJuniorFromMid(Junior junior)
+    {
+        if (junior == null)
+            throw new ArgumentNullException(nameof(junior));
+
+        if (!_supervisedJuniors.Contains(junior))
+            return;
+
+        _supervisedJuniors.Remove(junior);
+        junior.RemoveMidFromJuniorReverse(this);
+    }
+    internal void AddJuniorToMidReverse(Junior junior)
+    {
+        _supervisedJuniors.Add(junior);
+    }
+
+   
+    internal void RemoveJuniorFromMidReverse(Junior junior)
+    {
+        _supervisedJuniors.Remove(junior);
+    }
+}
---
+++ SpaCenter/Senior.cs
@@ -0,0 +1,44 @@
+﻿namespace SpaCenter;
+
+public class Senior
+{
+    private static List<Senior> seniors_List = new List<Senior>();
+    public static IReadOnlyList<Senior> Seniors => seniors_List.AsReadOnly();
+
+    private decimal _bonusCoefficient;
+
+    public decimal BonusCoefficient
+    {
+        get => _bonusCoefficient;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("BonusCoefficient cannot be less than 0.");
+
+            _bonusCoefficient = value;
+        }
+    }
+
+    public Senior(decimal bonusCoefficient)
+    {
+        BonusCoefficient = bonusCoefficient;
+        AddSenior(this);
+    }
+
+    private static void AddSenior(Senior senior)
+    {
+        if (senior == null)
+            throw new ArgumentException("Senior cannot be null");
+
+        seniors_List.Add(senior);
+    }
+
+    public static void LoadExtent(IEnumerable<Senior>? list)
+    {
+        seniors_List.Clear();
+
+        if (list == null) return;
+
+        seniors_List.AddRange(list);
+    }
+}
